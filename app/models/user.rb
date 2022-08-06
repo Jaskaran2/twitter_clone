5c2ad8b816890c2b54ee711915bf2ee5ab2331ff
Jaskaran2 <jaskaran@kiprosh.com>
2022-08-06 15:21:00 +0530

    Refactored and update follow count realtime

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   include NotificationHelper
+  include LikePost
+  include FollowUser
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :tweets, dependent: :destroy
@@ -21,73 +23,5 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-    
-
-  def follow(user)
-    active_friendships.create(followed_id: user.id)
-    #create notification for follow
-    notification=Notification.create(recipient:user,actor:Current.user,action:"followed",notifiable:user)
-    NotificationRelayJob.perform_later(notification)
-
-    notify(notification)
-
-  end
-
-  def unfollow(user)
-    active_friendships.find_by(followed_id: user.id).destroy
-      #create notification for unfollow
-      notification=Notification.create(recipient:user,actor:Current.user,action:"unfollowed",notifiable:user)
-      NotificationRelayJob.perform_later(notification)
-
-      notify(notification)
-
-  end
-
-  def following?(user)
-    following.include?(user)
-  end
-
-
-  def liked?(tweet)
-    liked_tweets.include?(tweet)
-  end
-
-  def liked_comment?(comment)
-    liked_comments.include?(comment)
-  end
-
-  def like(tweet)
-    if liked_tweets.include?(tweet)
-      liked_tweets.destroy(tweet)
-        #create unlike notification
-        notification=Notification.create(recipient:tweet.user,actor:Current.user,action:"unlike",notifiable:tweet)
-        NotificationRelayJob.perform_later(notification)
-    else
-      liked_tweets<<tweet
-        #create like notification
-        notification=Notification.create(recipient:tweet.user,actor:Current.user,action:"like",notifiable:tweet)
-        NotificationRelayJob.perform_later(notification)
-    end 
-    
-    notify(notification)
-    public_target="tweet_#{tweet.id}_public_likes"
-    broadcast_replace_later_to "public_likes",
-                                target:public_target,
-                                partial:"likes/like_count",
-                                locals:{tweet:tweet}
-  end
-
-
-  def like_comment(comment)
-    if liked_comments.include?(comment)
-      liked_comments.destroy(comment)
-    else
-      liked_comments<<comment
-    end
-  end
-
-
-
 
 end
