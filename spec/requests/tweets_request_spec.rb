6acbfa21f28d97e55e6d9dcbd69001b41121049b
Jaskaran2 <jaskaran@kiprosh.com>
2022-08-28 16:27:53 +0530

    Code cleanup, Usinf factory

--- spec/requests/tweets_request_spec.rb
+++ spec/requests/tweets_request_spec.rb
@@ -3,17 +3,18 @@ require "rails_helper"
 RSpec.describe "Creating articles" do
   
     context "Creating tweet" do
+        
         before{ post '/users',:params => {:user => {:name=>"Test",:username =>"test123", :email=> "test121@gmail.com",:password=>"123456",:password_confirmation=>"123456" } }}
         let!(:tweet){create(:tweet)}
 
-        it "type tweet" do
+        it "has type tweet" do
             number_of_tweets=Tweet.count
             post '/tweets',:params => { :tweet => { :body=> "Test"} }, as: :turbo_stream
             expect(Tweet.last.tweet_type).to eq("tweet")
             expect(Tweet.count).to eq(number_of_tweets+1)
         end
 
-        it "type retweet" do
+        it "has type retweet" do
             number_of_tweets=Tweet.count
             post "/tweets/#{tweet.id}/retweet",:params => { :body=> nil, :parent_tweet_id=> tweet.id}, as: :turbo_stream 
             expect(Tweet.last.tweet_type).to eq("retweet") 
@@ -21,7 +22,7 @@ RSpec.describe "Creating articles" do
         end
 
 
-        it "type reply" do
+        it "has type reply" do
             number_of_tweets=Tweet.count
             post "/tweets/#{tweet.id}/reply",:params => { :body=> "Its a reply", :parent_tweet_id=> tweet.id}, as: :turbo_stream 
             expect(Tweet.last.tweet_type).to eq("reply") 
