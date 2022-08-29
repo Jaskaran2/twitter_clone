d06555844b320cccd11a1c7cb7bc0284a33cb0ab
Jaskaran2 <jaskaran@kiprosh.com>
2022-08-29 10:18:01 +0530

    Added associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_065155) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_28_160823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,8 +77,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_26_065155) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "parent_tweet_id"
+    t.integer "reply_id"
     t.string "tweet_type", default: "tweet"
+    t.integer "retweet_id"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
