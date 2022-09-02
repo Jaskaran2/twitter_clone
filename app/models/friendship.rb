e3be13709981a3490145db68d7c104179dfc4488
Jaskaran2 <jaskaran@kiprosh.com>
2022-09-02 08:27:38 +0530

    Delete like and follow notification

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -6,15 +6,15 @@ class Friendship < ApplicationRecord
     validates :follower_id,presence: true
     validates :followed_id,presence: true
 
-    after_destroy_commit :send_unfollow_notification
+    after_destroy_commit :delete_follow_notification
 
     after_create_commit :send_follow_notification
 
 
-    def send_unfollow_notification
-        notification=Notification.create(recipient: self.followed,actor: self.follower,action: "unfollowed",notifiable: self.followed)
-        NotificationRelayJob.perform_later(notification)
-        notify(notification)
+    def delete_follow_notification
+        Notification.where(action:"followed")
+        .where(notifiable_id: self.followed.id)
+        .where(actor: Current.user).delete_all
     end
 
     def send_follow_notification
